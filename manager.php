5f263f9bfbd5b99416a1f0b0cc84edd91a9e97d3
bamadesigner <bamadesigner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-29 21:44:07 +0000

    fixing a quick bug
    git-svn-id: https://plugins.svn.wordpress.org/cpt-onomies/trunk@512570 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- manager.php
+++ manager.php
@@ -765,7 +765,7 @@ class CPT_ONOMIES_MANAGER {
 									
 								// make sure post types exist
 								foreach( $cpt[ 'attach_to_post_type' ] as $attached_key => $attached_post_type ) { 
-									if ( !post_type_exists( $attached_post_type ) )
+									if ( !post_type_exists( $attached_post_type ) && !array_key_exists( $attached_post_type, $this->user_settings[ 'custom_post_types' ] ) )
 										unset( $cpt[ 'attach_to_post_type' ][ $attached_key ] );
 								}
 								
