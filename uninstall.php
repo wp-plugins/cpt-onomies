63ae59cc79a4f342c2d1dff09b4afdadb8e70ada
bamadesigner <bamadesigner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-28 20:23:52 +0000

    The new uninstall is good to go.
    git-svn-id: https://plugins.svn.wordpress.org/cpt-onomies/trunk@719567 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -3,17 +3,29 @@
 global $wpdb;
 
 // if uninstall not called from the WordPress exit
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 	exit();
 	
-// delete options
-delete_option( 'custom_post_type_onomies_custom_post_types' );
-delete_option( 'custom_post_type_onomies_other_custom_post_types' );
+// delete individual site options and postmeta
+$blogs = $wpdb->get_col( $wpdb->prepare( "SELECT blog_id FROM " . $wpdb->blogs . " ORDER BY blog_id", NULL ) );
+foreach( $blogs as $this_blog_id ) {
+
+	// set blog id so $wpdb will know which table to tweak
+	$wpdb->set_blog_id( $this_blog_id );
+	
+	// delete site options
+	$wpdb->delete( $wpdb->options, array( 'option_name' => 'custom_post_type_onomies_custom_post_types' ) );
+	$wpdb->delete( $wpdb->options, array( 'option_name' => 'custom_post_type_onomies_other_custom_post_types' ) );
+	
+	// delete post meta
+	$wpdb->delete( $wpdb->postmeta, array( 'meta_key' => '_custom_post_type_onomies_relationship' ) );
+	
+}
+
+// delete network options
+delete_site_option( 'custom_post_type_onomies_custom_post_types' );
 
 // delete user options
-$wpdb->query( $wpdb->prepare( "DELETE FROM " . $wpdb->prefix . "usermeta WHERE meta_key IN ( 'wp_custom_post_type_onomies_show_edit_tables', 'custom_post_type_onomies_show_edit_tables' )", NULL ) );
+$wpdb->query( $wpdb->prepare( "DELETE FROM " . $wpdb->usermeta . " WHERE meta_key IN ( 'custom_post_type_onomies_dismiss', 'wp_custom_post_type_onomies_show_edit_tables', 'custom_post_type_onomies_show_edit_tables' )", NULL ) );
 	
-// delete postmeta
-$wpdb->query( $wpdb->prepare( "DELETE FROM " . $wpdb->prefix . "postmeta WHERE meta_key = %s", '_custom_post_type_onomies_relationship' ) );
-
 ?>
