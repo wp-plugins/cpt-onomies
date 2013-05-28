122d6ee80777f3673c0cb2e3970aef6c9d37aad3
bamadesigner <bamadesigner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-28 21:12:19 +0000

    Added ability to detect network admin over AJAX call.
    git-svn-id: https://plugins.svn.wordpress.org/cpt-onomies/trunk@719598 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/admin-options-validate.js
+++ js/admin-options-validate.js
@@ -24,7 +24,7 @@ jQuery.noConflict()(function(){
 		$post_type_name_exists = cpt_onomies_admin_options_L10n.post_type_name_exists;
 	else
 		$post_type_name_exists = 'That post type name already exists. Please choose another name.';
-	
+		
 	// validate custom post type name to make sure post type doesnt already exist
 	jQuery.validator.addMethod( 'custom_post_type_onomies_validate_name', function( value, element ) {
 		var validator = this, response;
@@ -35,6 +35,7 @@ jQuery.noConflict()(function(){
 			cache: false,
 			data: {
 				action: 'custom_post_type_onomy_validate_if_post_type_exists',
+				custom_post_type_onomies_is_network_admin: jQuery( "#custom-post-type-onomies-is-network-admin" ).val(),
 				original_custom_post_type_onomies_cpt_name: jQuery( "#custom-post-type-onomies-custom-post-type-original-name" ).val(),
 				custom_post_type_onomies_cpt_name: value
 			},
