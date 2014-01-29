cc88f66bf8c80629c1c341e6781000ce62f65aea
bamadesigner <bamadesigner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-29 01:22:55 +0000

    Just cleaning up the plugin file header and the license info.
    git-svn-id: https://plugins.svn.wordpress.org/cpt-onomies/trunk@847369 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cpt-onomies.php
+++ cpt-onomies.php
@@ -1,30 +1,29 @@
 <?php
 
-/*
-Plugin Name: CPT-onomies: Using Custom Post Types as Taxonomies
-Plugin URI: http://wordpress.org/plugins/cpt-onomies/
-Description: A CPT-onomy is a taxonomy built from a custom post type, using the post titles as the taxonomy terms. Create custom post types using the CPT-onomies custom post type manager or use post types created by themes or other plugins.
-Version: 1.3.1
-Author: Rachel Carden
-Author URI: http://wpdreamer.com
-Text Domain: cpt-onomies
-*/
+/**
+ * Plugin Name: CPT-onomies: Using Custom Post Types as Taxonomies
+ * Plugin URI: http://wordpress.org/plugins/cpt-onomies/
+ * Description: A CPT-onomy is a taxonomy built from a custom post type, using the post titles as the taxonomy terms. Create custom post types using the CPT-onomies custom post type manager or use post types created by themes or other plugins.
+ * Version: 1.3.1
+ * Author: Rachel Carden
+ * Author URI: http://wpdreamer.com
+ * Text Domain: cpt-onomies
+ */
 
-/*
-This program is free software; you can redistribute it and/or modify 
-it under the terms of the GNU General Public License as published by 
-the Free Software Foundation; version 2 of the License.
-
-This program is distributed in the hope that it will be useful, 
-but WITHOUT ANY WARRANTY; without even the implied warranty of 
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-GNU General Public License for more details. 
-
-You should have received a copy of the GNU General Public License 
-along with this program; if not, write to the Free Software 
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA 
-*/
-
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License or later.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * Visit http://www.fsf.org to read the GNU General Public License or
+ * to learn more about the Free Software Foundation.
+ */
+ 
 // let's define some stuff. maybe we'll use it later
 define( 'CPT_ONOMIES_VERSION', '1.3.1' );
 define( 'CPT_ONOMIES_WORDPRESS_MIN', '3.1' );
